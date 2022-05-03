b6a8a73a488a3491ca6bbd8b24f802237594e083
Tim <0xtimc@gmail.com>
2022-05-03 15:57:34 +0100

    Remove current docs

--- build.sh
+++ build.sh
@@ -6,10 +6,6 @@ cd 3.0;
 mkdocs build;
 cd ..;
 
-cd 4.0;
-mkdocs build;
-cd ..;
-
 cd 1.5;
 couscous generate;
 cd ..;
@@ -22,8 +18,7 @@ cd .. && pip install leaf-pygment/dist/leaf-0.1.0-dev.tar.gz
 
 mv 2.0/site site/2.0;
 mv 3.0/site site/3.0;
-mv 4.0/site site/4.0;
 mv 1.5/.couscous/generated site/1.5;
 
-echo "<meta http-equiv=\"refresh\" content=\"0; url=/4.0/\">" > site/index.html;
+echo "<meta http-equiv=\"refresh\" content=\"0; url=/3.0/\">" > site/index.html;
 
